39ed0c2a35b5390659403d4c36cd8586487dc415
mamau <mopckou_pak48rus@mail.ru>
2020-10-28 00:19:56 +0200

    дописаны тесты на live

--- stream/live_test.go
+++ stream/live_test.go
@@ -1,6 +1,8 @@
 package stream
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSetStream(t *testing.T) {
 	streamName := "someStream"
@@ -12,13 +14,28 @@ func TestSetStream(t *testing.T) {
 		t.Error("Error setting stream")
 	}
 
-	currentStream, err := live.GetStream(streamName)
+	if strm.Name != streamName {
+		t.Errorf("Stream doesnt have stream with name %v\n", strm.Name)
+	}
+}
+
+func TestGetStream(t *testing.T) {
+	streamName := "someStream"
+	strm := NewStream()
+	strm.Name = streamName
+	live := NewLive()
+
+	err := live.SetStream(strm)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
+	currentStream, err := live.GetStream(streamName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 	if currentStream.Name != streamName {
-		t.Errorf("Stream doesnt have stream with name %v\n", currentStream.Name)
+		t.Errorf("stream %v doesn`t exist", streamName)
 	}
 }
 
@@ -40,5 +57,29 @@ func TestAllStreams(t *testing.T) {
 		if value.Name != val.Name {
 			t.Errorf("Value %v at key %v is not same. %v != %v", val.Name, key, value.Name, val.Name)
 		}
+	}
+}
+
+func TestDeleteStream(t *testing.T) {
+	streamName := "someStream"
+	live := NewLive()
+	strm := NewStream()
+	strm.Name = streamName
+
+	if err := live.SetStream(strm); err != nil {
+		t.Error("Error setting stream")
+	}
+
+	deletedStream, err := live.DeleteStream(streamName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if deletedStream.Name != streamName {
+		t.Fatalf("Deleted stream %v not same", streamName)
+	}
+
+	_, err = live.GetStream(streamName)
+	if err == nil {
+		t.Errorf("Stream %v is not deleted", streamName)
 	}
 }
