aff66856ca22b713575298f8548e466c4dbb6b5d
chernikov <mamau4eg@gmail.com>
2021-02-12 17:50:48 +0200

    добавлен сентри

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
 	"github.com/mamau/restream/server"
+	"log"
 )
 
 func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://86a6e750f009479ca2aba656eeeb035a@o195631.ingest.sentry.io/5634880",
+	})
+	if err != nil {
+		log.Println("sentry.Init: %s", err)
+	}
 	server.Start()
 }
